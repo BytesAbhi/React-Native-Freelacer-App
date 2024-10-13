e3c7ebf02eca5ea293debd55044ec9c7efe22dd5
Manshu777 <manshusmartboy@gmail.com>
2024-10-13 12:20:28 +0530

    newme

---
+++ app/login/index.js
@@ -0,0 +1,22 @@
+// /app/login.js
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Login() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+      <Button title="Go to Signup" onPress={() => router.push('/signup')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
